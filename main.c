3734b6d190207a3d236276f89e1f923c705d6fc2
Kai Pereira <kaipereira2020@gmail.com>
2024-10-22 08:04:32 -0700

    backup

--- main.c
+++ main.c
@@ -17,19 +17,84 @@ enum Color {
 };
 
 enum Type {
-	empty = 'o',
-	pawn = 'p',
-	knight = 'n',
-	bishop = 'b',
-	rook = 'r',
-	queen = 'q',
-	king = 'k'
+	empty,
+	pawn,
+	knight,
+	bishop,
+	rook,
+	queen,
+	king 
 };
 
 struct Piece {
 	enum Color color;
 	enum Type type;
 };
+
+
+/*** Helper Functions ***/
+void set_piece(struct Piece board[ROW][COL], int row, int col, enum Color color, enum Type type) {
+	board[row][col].color = color;
+	board[row][col].type = type;
+}
+
+enum Type get_type_enum(char piece) {
+	switch(tolower(piece)) {
+		case 'p': return pawn;
+		case 'n': return knight;
+		case 'b': return bishop;
+		case 'r': return rook;
+		case 'q': return queen;
+		case 'k': return king;
+		default: return empty;
+	}
+}
+
+char get_type_char(enum Type type) {
+	char piece;
+
+	switch(type) {
+		case pawn: piece = 'p'; break;
+		case knight: piece = 'n'; break;
+		case bishop: piece = 'b'; break;
+		case rook: piece = 'r'; break;
+		case queen: piece = 'q'; break;
+		case king: piece = 'k'; break;
+		case empty: piece = '.'; break;
+		default: piece = '?';
+	}
+
+	return piece;
+}
+
+char get_color_char(enum Color color) {
+	char p_color;
+
+	switch(color) {
+		case white: p_color = 'w'; break;
+		case black: p_color = 'b'; break;
+		case none: p_color = 'n'; break;
+		default: p_color = '?'; break;
+	}
+
+	return p_color;
+}
+
+void print_board(struct Piece board[COL][ROW]) {
+	for (size_t r = 0; r < ROW; r++) {
+		for (size_t c = 0; c < COL; c++) {
+			struct Piece square = board[r][c];
+
+			char color = square.color;
+			char type = get_type_char(square.type);
+
+			char f_type = color == white ? toupper(type) : type;
+
+			printf("%c", f_type);
+		}
+		printf("\n");
+	}
+}
 
 
 /** Functionality ***/
@@ -56,37 +121,22 @@ void format(char fen[]) {
 	struct Piece board[ROW][COL];
 
 	for (i = 0; i < BOARD_SIZE; i++) {
-		char square = pieces[i];
+		char piece = pieces[i];
 		
 		int row = i / ROW;
 		int col = i % COL;
 
-		struct Piece piece = board[row][col];
-		
-		// printf("%c", square);
-
-		if (square == '.') {
-			piece.color = none;
-			piece.type = empty;
+		if (piece == '.') {
+			set_piece(board, row, col, none, empty);
 		} else {
-			if (isupper(square)) piece.color = black;
-			else piece.color = white;
-			
-			printf("%c", tolower(square));
-			piece.type = tolower(square);
+			enum Color color = isupper(piece) ? white : black;
+			enum Type type = get_type_enum(piece);
+
+			set_piece(board, row, col, color, type);
 		}
 	}
 
-	printf("\n");
-
-	printf("%c", board[0][0].type);
-	
-	/***	
-	for (size_t a = 0; a < ROW; a++) {
-		for (size_t b = 0; b < COL; b++) {
-			printf("%c", board[a][b].type);	
-		}
-	} ***/
+	print_board(board);
 }
 
 
