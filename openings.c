b25c048b44ac4fe59d7d639b722f037bf29694af
Kai Pereira <kaipereira2020@gmail.com>
2025-01-16 07:41:14 -0800

    some good stuff

--- openings.c
+++ openings.c
@@ -3,28 +3,12 @@
 #include <string.h>
 #include "openings.h"
 
-
-void parse_csv_line(char *line, struct Opening *opening) {
-	char *token;
-	char *string = line;
-
-	token = strtok_r(string, ",", &string);
-	if (token) strncpy(opening->eco, token, sizeof(opening->eco));
-
-	if (string[0] == '"') {
-		token = strtok_r(string + 1, "\"", &string);
-	} else {
-		strtok_r(string, ",", &string);
-	}
-
-	if (token) strncpy(opening->name, token, sizeof(opening->name));
-
-	opening->move_count = 0;
-	token = strtok_r(string, " ", &string);
-	while (token && opening->move_count < MAX_MOVES) {
-		strncpy(opening->moves[opening->move_count++], token, 10);
-		token = strtok_r(string, " ", &string);
-	}
+void append_char(char *s, char c) {
+    while (*s++);
+  
+    *(s - 1) = c;
+  
+    *s = '\0';
 }
 
 void parse_openings(struct Opening *opening) {
@@ -46,9 +30,37 @@ void parse_openings(struct Opening *opening) {
 	int line_count = 0;
 
 	while (fgets(line, sizeof(line), file) && line_count < MAX_LINES) {
+		int token = 0;
+		char eco[sizeof(opening->eco)];
+		char name[sizeof(opening->name)];
+		char opening[sizeof(opening->moves)];
+
 		for (int i = 0; i < MAX_LINE_LENGTH; i++) {
-			if (!line[i]) break;
-			printf("%c", line[i]);
+			char character = line[i];
+
+			if (!character) break;
+
+			if (character = ',') {
+ 				token++; 
+				continue;
+			}
+
+			printf("MAKES IT HERE");
+
+			switch (token) {
+				case 0:
+					append_char(eco, character);
+					printf(" %c ", character);
+					break;
+				case 1:
+					append_char(name, character);
+					printf(" %c ", character);
+					break;
+				case 2:
+					append_char(name, character);
+					printf(" %c ", character);
+					break;
+			}
 		}
 
 		printf("\n");
@@ -56,8 +68,6 @@ void parse_openings(struct Opening *opening) {
 
 	fclose(file);
 
-
-	printf("MAKES IT HERE %d \n", line_count);
 
 	for (int i = 0; i < line_count; i++) {
 		printf("ECO: %s, NAME: %s, MOVES:", openings[i].eco, openings[i].name);
